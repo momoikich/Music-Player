a411eb149542eb4c0768e73d5a4e8b84385b1443
momoikich <mohamed.ikich@etu.toulouse-inp.fr>
2023-12-17 18:50:29 +0100

    uppdate the dashboard

--- music_player.py
+++ music_player.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import pygame
+import eyed3
 
 app = Flask(__name__)
 
@@ -10,7 +11,8 @@ class MusicPlayer:
         self.playlist = []
         self.current_song_index = 0
         self.paused = False
-        self.volume = 0.5  # Initial volume level (0.0 to 1.0)
+        self.volume = 0.5
+        self.paused_data = None
 
     def load_songs(self, folder_path):
         self.playlist = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.lower().endswith('.mp3')]
@@ -44,10 +46,21 @@ class MusicPlayer:
         self.play()
 
     def set_volume(self, volume):
-        self.volume = float(volume)  # Convert to float
+        self.volume = float(volume)
         pygame.mixer.music.set_volume(self.volume)
         print(f"Volume set to: {self.volume * 100}%")
 
+    def get_total_duration(self):
+        if self.playlist:
+            audiofile = eyed3.load(self.playlist[self.current_song_index])
+            if audiofile:
+                return int(audiofile.info.time_secs)
+        return 0
+
+    def get_current_progress(self):
+        if self.playlist and pygame.mixer.music.get_busy():
+            return pygame.mixer.music.get_pos() / 1000
+        return 0
 
 player = MusicPlayer()
 
@@ -68,13 +81,17 @@ def play():
 
 @app.route('/pause')
 def pause():
+    current_progress = player.get_current_progress()
     player.pause()
-    return redirect(url_for('index'))
+    total_duration = player.get_total_duration()
+    player.paused_data = {'progress': current_progress, 'duration': total_duration}
+    return jsonify(player.paused_data)
 
 @app.route('/resume')
 def resume():
-    player.resume()
-    return redirect(url_for('index'))
+    if player.paused_data:
+        player.resume()
+    return jsonify(player.paused_data)
 
 @app.route('/stop')
 def stop():
@@ -91,8 +108,23 @@ def set_volume(volume):
     player.set_volume(volume)
     return redirect(url_for('index'))
 
+@app.route('/progress', methods=['GET'])
+def progress():
+    if player.paused and player.paused_data:
+        # If paused, return the last known progress
+        return jsonify(progress=player.paused_data['progress'], duration=player.paused_data['duration'])
+    
+    # Retrieve the current progress and duration from your music player or server
+    current_progress = player.get_current_progress()
+    total_duration = player.get_total_duration()
+
+    # Return the progress and duration as JSON
+    return jsonify(progress=current_progress, duration=total_duration)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
