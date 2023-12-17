cd1b836299df45a057ba2d96f07e6726095e891a
momoikich <mohamed.ikich@etu.toulouse-inp.fr>
2023-12-17 06:10:42 +0100

    web app

--- music_player.py
+++ music_player.py
@@ -1,7 +1,8 @@
 import os
-import tkinter as tk
-from tkinter import filedialog
+from flask import Flask, render_template, request, redirect, url_for
 import pygame
+
+app = Flask(__name__)
 
 class MusicPlayer:
     def __init__(self):
@@ -12,7 +13,6 @@ class MusicPlayer:
         self.volume = 0.5  # Initial volume level (0.0 to 1.0)
 
     def load_songs(self, folder_path):
-        # Load all MP3 files from the specified folder
         self.playlist = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.lower().endswith('.mp3')]
         if not self.playlist:
             print("No MP3 files found in the specified folder.")
@@ -44,58 +44,60 @@ class MusicPlayer:
         self.play()
 
     def set_volume(self, volume):
-        self.volume = volume
-        pygame.mixer.music.set_volume(volume)
-        print(f"Volume set to: {volume * 100}%")
+        self.volume = float(volume)  # Convert to float
+        pygame.mixer.music.set_volume(self.volume)
+        print(f"Volume set to: {self.volume * 100}%")
 
-class MusicPlayerApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Music Player")
 
-        self.player = MusicPlayer()
+player = MusicPlayer()
 
-        self.folder_path_var = tk.StringVar()
+@app.route('/')
+def index():
+    return render_template('index.html', player=player)
 
-        # Entry for folder path
-        entry_label = tk.Label(root, text="Enter Folder Path:")
-        entry_label.pack(pady=5)
-        folder_entry = tk.Entry(root, textvariable=self.folder_path_var, width=40)
-        folder_entry.pack(pady=5)
-        browse_button = tk.Button(root, text="Browse", command=self.browse_folder)
-        browse_button.pack(pady=5)
+@app.route('/load_songs', methods=['POST'])
+def load_songs():
+    folder_path = request.form['folder_path']
+    player.load_songs(folder_path)
+    return redirect(url_for('index'))
 
-        # Volume control
-        volume_label = tk.Label(root, text="Volume:")
-        volume_label.pack(pady=5)
-        volume_slider = tk.Scale(root, from_=0.0, to=1.0, resolution=0.1, orient=tk.HORIZONTAL, command=self.set_volume)
-        volume_slider.set(self.player.volume)
-        volume_slider.pack(pady=5)
+@app.route('/play')
+def play():
+    player.play()
+    return redirect(url_for('index'))
 
-        # Control buttons
-        play_button = tk.Button(root, text="Play", command=self.player.play)
-        play_button.pack(pady=5)
-        pause_button = tk.Button(root, text="Pause", command=self.player.pause)
-        pause_button.pack(pady=5)
-        resume_button = tk.Button(root, text="Resume", command=self.player.resume)
-        resume_button.pack(pady=5)
-        stop_button = tk.Button(root, text="Stop", command=self.player.stop)
-        stop_button.pack(pady=5)
-        next_button = tk.Button(root, text="Next Song", command=self.player.next_song)
-        next_button.pack(pady=5)
+@app.route('/pause')
+def pause():
+    player.pause()
+    return redirect(url_for('index'))
 
-    def browse_folder(self):
-        folder_path = filedialog.askdirectory()
-        self.folder_path_var.set(folder_path)
-        self.player.load_songs(folder_path)
+@app.route('/resume')
+def resume():
+    player.resume()
+    return redirect(url_for('index'))
 
-    def set_volume(self, volume):
-        self.player.set_volume(float(volume))
+@app.route('/stop')
+def stop():
+    player.stop()
+    return redirect(url_for('index'))
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = MusicPlayerApp(root)
-    root.mainloop()
+@app.route('/next_song')
+def next_song():
+    player.next_song()
+    return redirect(url_for('index'))
+
+@app.route('/set_volume/<float:volume>')
+def set_volume(volume):
+    player.set_volume(volume)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
 
 
 
